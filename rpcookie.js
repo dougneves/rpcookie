6a0f8d156213ed4c25a38572fbf8dfa36850d8ea
Douglas Neves <dougneves@gmail.com>
2017-04-27 08:43:10 -0300

    adding lambda and removing ;

--- rpcookie.js
+++ rpcookie.js
@@ -4,8 +4,7 @@
  *   Copyright (c) 2016-2099 Bruno Jesus
  *   Released under the MIT license
  */
-var rpcookie =
-{
+const rpcookie = {
   /*
    *  prefix    cookie name prefix
    *  pages     internal list of all opened pages
@@ -18,219 +17,206 @@ var rpcookie =
    */
 
   /* ================================= STATE ================================ */
-  init: function(params)
-  {
+  init: (params) => {
     if (rpcookie.timer_ix)
-      return true;
+      return true
     if (typeof document.cookie != 'string' || !navigator.cookieEnabled)
-      return false;
+      return false
 
-    rpcookie.prefix = 'rpc_';
-    rpcookie.pages = {};
-    rpcookie.interval = 300;
-    rpcookie.loaded = false;
-    rpcookie.runs = 0;
+    rpcookie.prefix = 'rpc_'
+    rpcookie.pages = {}
+    rpcookie.interval = 300
+    rpcookie.loaded = false
+    rpcookie.runs = 0
 
     rpcookie.callbacks = { new: rpcookie.internal_new,
                            close: rpcookie.internal_close,
-                           load: rpcookie.internal_load };
+                           load: rpcookie.internal_load }
 
-    rpcookie.config(params);
+    rpcookie.config(params)
 
-    window.addEventListener('unload', rpcookie.unload_event);
+    window.addEventListener('unload', rpcookie.unload_event)
     if (!rpcookie.register_me(true))
-      return false;
+      return false
 
-    rpcookie.timer_ix = setInterval(rpcookie.timer_func, rpcookie.interval);
+    rpcookie.timer_ix = setInterval(rpcookie.timer_func, rpcookie.interval)
     if (!rpcookie.timer_ix) /* insanity check */
-      rpcookie.del_cookie(rpcookie.my_name);
+      rpcookie.del_cookie(rpcookie.my_name)
 
-    return !!rpcookie.timer_ix;
+		console.log(rpcookie)
+
+    return !!rpcookie.timer_ix
   },
-  
-  config: function(params)
-  {
+
+  config: (params) => {
     if (typeof params != 'object')
-      return false;
+      return false
 
     if (typeof params.prefix == 'string')
-      rpcookie.prefix = params.prefix;
+      rpcookie.prefix = params.prefix
     if (typeof params.interval == 'number')
-      rpcookie.interval = params.interval;
+      rpcookie.interval = params.interval
 
     if (typeof params.new_page == 'function')
-      rpcookie.callbacks.new = params.new_page;
+      rpcookie.callbacks.new = params.new_page
     if (typeof params.close_page == 'function')
-      rpcookie.callbacks.close = params.close_page;
+      rpcookie.callbacks.close = params.close_page
     if (typeof params.load_done == 'function')
-      rpcookie.callbacks.load = params.load_done;
+      rpcookie.callbacks.load = params.load_done
 
-    return true;
+    return true
   },
 
-  register_me: function(rename)
-  {
+  register_me: (rename) => {
     if (rename)
-      rpcookie.my_name = rpcookie.prefix+rpcookie.random();
+      rpcookie.my_name = rpcookie.prefix+rpcookie.random()
     if (!rpcookie.add_cookie(rpcookie.my_name, '%'))
-      return false;
+      return false
 
-    var page = rpcookie.register_page(rpcookie.my_name);
-    page.is_new = false; /* do not callback for ourself */
+    var page = rpcookie.register_page(rpcookie.my_name)
+    page.is_new = false /* do not callback for ourself */
 
-    rpcookie.update_me();
-    return true;
+    rpcookie.update_me()
+    return true
   },
 
-  stop: function()
-  {
+  stop: () => {
     if (rpcookie.timer_ix)
     {
-      clearInterval(rpcookie.timer_ix);
-      rpcookie.timer_ix = false;
-      rpcookie.del_cookie(rpcookie.my_name);
+      clearInterval(rpcookie.timer_ix)
+      rpcookie.timer_ix = false
+      rpcookie.del_cookie(rpcookie.my_name)
     }
-    return true;
+    return true
   },
 
-  internal_load: function()
-  {
-    console.log('load  ok');
+  internal_load: () => {
+    console.log('load  ok')
   },
 
-  internal_new: function(page)
-  {
-    console.log('opened:', page.url);
+  internal_new: (page) => {
+    console.log('opened:', page.url)
   },
 
-  internal_close: function(page)
-  {
-    console.log('closed:', page.url);
+  internal_close: (page) => {
+    console.log('closed:', page.url)
   },
   /* ======================================================================== */
 
   /* ================================ PUBLIC ================================ */
-  list_pages: function()
-  {
-    var i, keys, ret;
+  list_pages: () => {
+    var i, keys, ret
 
     for (i = 0, keys = Object.keys(rpcookie.pages), ret = []; i < keys.length; i++)
-      ret.push(rpcookie.make_data(keys[i]));
+      ret.push(rpcookie.make_data(keys[i]))
 
-    return ret;
+    return ret
   },
   /* ======================================================================== */
 
   /* ================================ WORKER ================================ */
-  make_data: function(name)
-  {
+  make_data: (name) => {
     return { 'url': rpcookie.pages[name].url,
              'id': name.substring(rpcookie.prefix.length) }
   },
 
-  timer_func: function(me)
-  {
-    var cookies = rpcookie.parse_cookies(), keys, i, checked, page;
+  timer_func: () => {
+    var cookies = rpcookie.parse_cookies(), keys, i, checked, page
 
-    for (i = 0, keys = Object.keys(cookies), checked = []; i < keys.length; i++)
-    {
+    for (i = 0, keys = Object.keys(cookies), checked = []; i < keys.length; i++) {
       /* add and update every page reading the cookie data */
       if (typeof rpcookie.pages[keys[i]] == 'undefined')
-        page = rpcookie.register_page(keys[i]);
+        page = rpcookie.register_page(keys[i])
       else
-        page = rpcookie.pages[keys[i]];
+        page = rpcookie.pages[keys[i]]
 
-      page.val = cookies[keys[i]];
-      rpcookie.parse_page(keys[i]);
+      page.val = cookies[keys[i]]
+      rpcookie.parse_page(keys[i])
 
       /* when the page is inactive the browser will fire the event only every 1000ms,
        * so wait a least 2 cycles before killing the page */
       if (page.bad_runs > 2500 / rpcookie.interval)
-        rpcookie.del_cookie(keys[i]);
+        rpcookie.del_cookie(keys[i])
       else
-        checked.push(keys[i]);
+        checked.push(keys[i])
     }
 
     for (i = 0, keys = Object.keys(rpcookie.pages); i < keys.length; i++)
     {
-      page = rpcookie.pages[keys[i]];
+      page = rpcookie.pages[keys[i]]
       /* callback dead pages */
       if (keys[i] != rpcookie.my_name && rpcookie.in_array(keys[i], checked) < 0)
       {
-        delete rpcookie.pages[keys[i]];
-        rpcookie.callbacks.close(page);
+        delete rpcookie.pages[keys[i]]
+        rpcookie.callbacks.close(page)
       }
       /* callback new pages */
       else if (page.is_new)
       {
-        page.is_new = false;
+        page.is_new = false
         if (rpcookie.loaded) /* already existing pages are not callback'ed */
-          rpcookie.callbacks.new(rpcookie.make_data(keys[i]));
+          rpcookie.callbacks.new(rpcookie.make_data(keys[i]))
       }
     }
 
-    rpcookie.runs++;
-    rpcookie.runs &= 7;
-    rpcookie.update_me();
+    rpcookie.runs++
+    rpcookie.runs &= 7
+    rpcookie.update_me()
 
     if (!rpcookie.loaded)
     {
-      rpcookie.callbacks.load();
-      rpcookie.loaded = true;
+      rpcookie.callbacks.load()
+      rpcookie.loaded = true
     }
   },
 
-  update_me: function()
-  {
-    var page = rpcookie.pages[rpcookie.my_name], str;
-    page.url = location.href;
+  update_me: () => {
+    var page = rpcookie.pages[rpcookie.my_name], str
+    page.url = location.href
     str = '@$' +
           'R' + rpcookie.runs + '$' +
-          'U' + page.url + '$';
-    page.val = str;
-    rpcookie.add_cookie(rpcookie.my_name, page.val);
+          'U' + page.url + '$'
+    page.val = str
+    rpcookie.add_cookie(rpcookie.my_name, page.val)
   },
 
-  parse_page: function(name)
-  {
-    var page = rpcookie.pages[name], data, runs, url;
+  parse_page: (name) => {
+    var page = rpcookie.pages[name], data, runs, url
 
-    data = page.val.split('$', 3);
+    data = page.val.split('$', 3)
     if (data.length != 3 || data[0] != '@')
-      return false;
-    runs = parseInt(data[1][1]);
+      return false
+    runs = parseInt(data[1][1])
     if (runs == page.runs)
-      page.bad_runs++;
+      page.bad_runs++
     else
-      page.bad_runs = 0;
-    page.runs = runs;
-    
-    url = data[2].substring(1);
+      page.bad_runs = 0
+    page.runs = runs
+
+    url = data[2].substring(1)
     if (page.url != url)
-      page.is_new = true;
-    page.url = url;
+      page.is_new = true
+    page.url = url
   },
 
-  register_page: function(name)
-  {
-    rpcookie.pages[name] = { is_new: true, url: '', runs: 0, bad_runs: 0 };
-    return rpcookie.pages[name];
+  register_page: (name) => {
+    rpcookie.pages[name] = { is_new: true, url: '', runs: 0, bad_runs: 0 }
+    return rpcookie.pages[name]
   },
   /* ======================================================================== */
 
   /* ============================ DOCUMENT COOKIE =========================== */
-  parse_cookies: function()
-  {
-    var cookies = {}, s, i, p, plen = rpcookie.prefix.length;
+  parse_cookies: () => {
+    var cookies = {}, s, i, p, plen = rpcookie.prefix.length
     if (typeof document.cookie == 'string')
     {
-      s = document.cookie.split(';');
+      s = document.cookie.split('')
       for (i = 0; i < s.length; i++)
       {
-        p = s[i].split('=');
-        if (p.length == 2 && (p[0] = rpcookie.trim(p[0])) && 
+        p = s[i].split('=')
+        if (p.length == 2 && (p[0] = rpcookie.trim(p[0])) &&
             p[0].substring(0,plen) == rpcookie.prefix)
-          cookies[p[0]] = p[1];
+          cookies[p[0]] = p[1]
       }
     }
 
@@ -240,56 +226,49 @@ var rpcookie =
       if (!rpcookie.register_me(false))
       {
         /* browser is blocking our cookies, we can no longer recover */
-        cookies = {};
-        rpcookie.stop();
+        cookies = {}
+        rpcookie.stop()
       }
     }
     else
-      delete cookies[rpcookie.my_name];
-    
-    return cookies;
+      delete cookies[rpcookie.my_name]
+
+    return cookies
   },
 
-  add_cookie: function(name, value)
-  {
-    document.cookie = name + '=' + value;
-    return document.cookie.indexOf(name) >= 0;
+  add_cookie: (name, value) => {
+    document.cookie = name + '=' + value
+    return document.cookie.indexOf(name) >= 0
   },
 
-  del_cookie: function(name)
-  {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+  del_cookie: (name) => {
+    document.cookie = name + '= expires=Thu, 01 Jan 1970 00:00:01 GMT'
   },
   /* ======================================================================== */
 
   /* ================================ HELPERS =============================== */
-  unload_event: function()
-  {
-    rpcookie.stop();
-  },
-  
-  page_visible: function()
-  {
-    return !(document.hidden || document.webkitHidden || document.mozHidden || document.msHidden);
+  unload_event: () => {
+    rpcookie.stop()
   },
 
-  in_array: function(what, where)
-  {
-    var i;
+  page_visible: () => {
+    return !(document.hidden || document.webkitHidden || document.mozHidden || document.msHidden)
+  },
+
+  in_array: (what, where) => {
+    var i
     for (i = 0; i < where.length; i++)
       if (what == where[i])
-        return i;
-    return -1;
+        return i
+    return -1
   },
 
-  trim: function(str) /* str.trim is too new on IE */
-  {
-    return str.replace(/^\s+|\s+$/gm,'');
+  trim: (str) => {
+    return str.replace(/^\s+|\s+$/gm,'') /* str.trim is too new on IE */
   },
 
-  random: function()
-  {
-    return Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
+  random: () => {
+    return Math.floor(Math.random() * (999999 - 100000 + 1) + 100000)
   }
   /* ======================================================================== */
-};
+}
